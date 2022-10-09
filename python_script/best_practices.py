470d28bbd76b00ec40825f30fa6b489d35d0f131
htunnthuthu <martin.mystery9@gmail.com>
2022-10-10 00:15:02 +0800

    python main function

---
+++ python_script/best_practices.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from dataclasses import dataclass
+from time import sleep
+
+print("This is my file to demonstrate best practices.")
+
+def process_data(data):
+    print("Beginning data processing...")
+    modified_data = data + " that has been modified"
+    sleep(3)
+    print("Data processing finished.")
+    return modified_data
+
+def read_data_from_web():
+    print("Reading data from the web")
+    data = "Data from the web"
+    return data
+
+def write_data_to_database(data):
+    print("Writing data to a database")
+    print(data)
+
+def main():
+    data = read_data_from_web()
+    modified_data = process_data(data)
+    write_data_to_database(modified_data)
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ python_script/execution_methods.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+print("This is my file to test python's execution methods.")
+print("The variable __name__ tells me which context this file is running in.")
+print("The value of __name__ is:", repr(__name__))
